16b3703eb575346d394695df780761a25ecf65a7
WizardCR <facundofischer@gmail.com>
2025-02-19 19:49:39 +0000

    test

---
+++ script.js
@@ -0,0 +1,34 @@
+function initMap() {
+    var mapCenter = { lat: 9.935, lng: -84.091 };
+
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: mapCenter,
+        mapTypeId: 'roadmap'
+    });
+
+    var locations = [
+        { lat: 9.9281, lng: -84.0907, title: "Location 1", img: "custom-pin1.png" },
+        { lat: 9.935, lng: -84.100, title: "Location 2", img: "custom-pin2.png" }
+    ];
+
+    locations.forEach(function (location) {
+        var marker = new google.maps.Marker({
+            position: { lat: location.lat, lng: location.lng },
+            map: map,
+            title: location.title,
+            icon: {
+                url: location.img, 
+                scaledSize: new google.maps.Size(40, 40)
+            }
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: `<h3>${location.title}</h3><img src="${location.img}" width="100">`
+        });
+
+        marker.addListener("click", function () {
+            infoWindow.open(map, marker);
+        });
+    });
+}
